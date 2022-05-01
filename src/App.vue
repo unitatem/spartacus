a69f00ac468d3aaa407de8f8bf5daf5dee198e47
Mariusz Wachowicz <wachowicz.mariusz@gmail.com>
2022-05-01 19:38:52 +0200

    Add Table component

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,18 @@
-<script>
-</script>
-
 <template>
   <h1>TEST</h1>
+  <Table />
+  <h1>END</h1>
 </template>
+
+<script>
+import Table from './components/Table.vue'
+
+export default {
+  components: {
+    Table
+  }
+}
+</script>
 
 <style>
 </style>
---
+++ src/components/Table.vue
@@ -0,0 +1,54 @@
+<template>
+  <table>
+    <tr>
+      <th>Ranking</th>
+      <th>Name</th>
+      <th>Elo</th>
+    </tr>
+    <tr v-for="player in players">
+      <td>{{ player.ranking }}</td>
+      <td>{{ player.name }}</td>
+      <td>{{ player.elo }}</td>
+    </tr>
+  </table>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      players: [
+        { ranking: 1, name: "Alice", elo: 2000 },
+        { ranking: 2, name: "Bob", elo: 1500 },
+        { ranking: 3, name: "Charlie", elo: 1000 }
+      ]
+    }
+  }
+}
+</script>
+
+<style scoped>
+table,
+th,
+td {
+  border: 1px solid;
+  border-collapse: collapse;
+}
+
+table {
+  width: 80%;
+}
+
+th {
+  background-color: #04AA6D;
+  color: white;
+}
+
+tr:nth-child(even) {
+  background-color: #f2f2f2;
+}
+
+tr:hover {
+  background-color: coral;
+}
+</style>
